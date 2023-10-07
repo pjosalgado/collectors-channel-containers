b692239fc3dba2705bb7a05ce3091ae08339a44c
Paulo Salgado <pjosalgado@gmail.com>
2023-10-07 12:14:26 -0300

    Updated docker-compose and init script

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo 'Starting MongoDB, InfluxDB and Grafana...'
-docker network create cc-network
-docker-compose -p cc-stack up -d
+docker network create collectors-network
+docker-compose -p collectors-stack up -d
 
 echo 'Configuring Grafana...'
 sleep 10
-docker exec -it grafana-cc grafana-cli admin reset-admin-password p455w0r4g
+docker exec -it grafana-collectors grafana-cli admin reset-admin-password p455w0r4g
